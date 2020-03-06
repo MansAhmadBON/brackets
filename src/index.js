ad4b4cc5c5105356d57691e0f34424f9d3db23b1
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2020-03-06 10:24:30 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const bracketsArr = bracketsConfig.map(item => item.join(''));
+    const bracketsLength = bracketsArr.length;
+
+    for(let i = 0; i < bracketsLength;){
+        let currentBrackets = bracketsArr[i];
+
+        if(str.indexOf(currentBrackets) !== -1){
+            str = str.replace(currentBrackets, '');
+            i = 0
+        } else {
+            i++
+        }
+
+    }
+
+    return str === '' ? true : false;
+};
